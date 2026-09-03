d325cd2dc2bbe3eeebefac61e7532ff831f7f8f8
D. Reddy Snehalatha <snehalatha9346@gmail.com>
2026-09-03 13:12:02 +0530

    Create types.h

---
+++ types.h
@@ -0,0 +1,8 @@
+typedef unsigned long int u32;
+typedef unsigned int u8;
+typedef unsigned char u1;
+typedef char s8;
+typedef int s32;
+typedef float f32;
+typedef unsigned short int u16;
+typedef short int s16;
